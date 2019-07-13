2dd91cc231a9863c05fbe48d480ffe0cffac2554
kyle <kyjohnso@gmail.com>
2019-07-13 12:34:38 +0100

    made a main function

--- load_games.py
+++ load_games.py
@@ -230,40 +230,42 @@ def add_game(game_url):
         game.save()
     return
 
-base_url = 'http://gd2.mlb.com/components/game/mlb/'
-
-recent_datetime_dict = Game.objects.all().aggregate(Max('start_datetime'))
-recent_datetime = recent_datetime_dict['start_datetime__max']
-if recent_datetime:
-    start_datetime = recent_datetime - datetime.timedelta(days=1)
-else:
-    start_datetime = datetime.datetime(2011, 3, 1, 0, 0, 0).replace(tzinfo=UTC)
-cur_datetime = start_datetime
-end_datetime = datetime.datetime.now().replace(tzinfo=UTC) + \
-               datetime.timedelta(days=1)
-
-while cur_datetime < end_datetime:
-    print 'loading games from ' + cur_datetime.date().isoformat()
-    day_url = base_url + 'year_%d/month_%02d/day_%02d' % (cur_datetime.year,
-                                                           cur_datetime.month,
-                                                           cur_datetime.day,)
-
-    try:
-        day_html = urllib2.urlopen(day_url).read()
-        day_soup = BeautifulSoup(day_html, 'html.parser')
-        game_links = [link.get('href') 
-                        for link in day_soup.find_all('a')
-                          if link.get('href').find('gid_') >= 0]
-    except urllib2.HTTPError:
-        game_links = []
-
-    for game_link in game_links:
-        game_url = day_url + '/' + game_link[game_link.find('/')+1:]
-        print 'game url: %s' % game_url
-        try:
-            add_game(game_url)
-        except:
-            print 'error reading game at: %s' % game_url
-            continue 
-
-    cur_datetime += datetime.timedelta(days=1)
+if __name__ == "__main__":
+  base_url = 'http://gd2.mlb.com/components/game/mlb/'
+  
+  recent_datetime_dict = Game.objects.all().aggregate(Max('start_datetime'))
+  recent_datetime = recent_datetime_dict['start_datetime__max']
+  if recent_datetime:
+      start_datetime = recent_datetime - datetime.timedelta(days=1)
+  else:
+      start_datetime = datetime.datetime(2011, 3, 1, 0, 0, 0).replace(
+                                                                tzinfo=UTC)
+  cur_datetime = start_datetime
+  end_datetime = datetime.datetime.now().replace(tzinfo=UTC) + \
+                 datetime.timedelta(days=1)
+  
+  while cur_datetime < end_datetime:
+      print 'loading games from ' + cur_datetime.date().isoformat()
+      day_url = base_url + 'year_%d/month_%02d/day_%02d' % (cur_datetime.year,
+                                                             cur_datetime.month,
+                                                             cur_datetime.day,)
+  
+      try:
+          day_html = urllib2.urlopen(day_url).read()
+          day_soup = BeautifulSoup(day_html, 'html.parser')
+          game_links = [link.get('href') 
+                          for link in day_soup.find_all('a')
+                            if link.get('href').find('gid_') >= 0]
+      except urllib2.HTTPError:
+          game_links = []
+  
+      for game_link in game_links:
+          game_url = day_url + '/' + game_link[game_link.find('/')+1:]
+          print 'game url: %s' % game_url
+          try:
+              add_game(game_url)
+          except:
+              print 'error reading game at: %s' % game_url
+              continue 
+  
+      cur_datetime += datetime.timedelta(days=1)
